cb2dc275474cbba94bae1e3ba070ab8651520a09
Daniel Bowen <djbowen95@gmail.com>
2022-06-09 16:44:27 +0100

    Give the product model an 'product_name' definition

--- models/Product.js
+++ models/Product.js
@@ -15,7 +15,15 @@ Product.init(
       primaryKey: true,
       autoIncrement: true
     },
-
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull:false
+      // Validate that type is a decimal
+    }
   },
   {
     sequelize,
