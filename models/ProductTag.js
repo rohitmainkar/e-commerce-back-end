1cf860ad1b6016d3ccb68eb0cab5f97c588854b5
Daniel Bowen <djbowen95@gmail.com>
2022-06-09 18:02:17 +0100

    Minor refactor to models, corrections to syntax

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -13,7 +13,7 @@ ProductTag.init(
       autoIncrement: true
     },
     product_id: {
-      type: DataType.INTEGER,
+      type: DataTypes.INTEGER,
       references: {
         model: 'product',
         key: 'id',
@@ -21,7 +21,7 @@ ProductTag.init(
       }
     },
     tag_id: {
-      type: DataType.INTEGER,
+      type: DataTypes.INTEGER,
       references: {
         model: 'tag',
         key: 'id',
