0e046abf800ff24c4968a70763d0bdda4a0120e0
Daniel Bowen <djbowen95@gmail.com>
2022-06-24 15:30:49 +0100

    Include all related products when categories are queried

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,18 +5,22 @@ const { Category, Product } = require('../../models');
 
 router.get('/', async (req, res) => {
   try {
-    const categoryData = await Category.findAll();
+    const categoryData = await Category.findAll({
+      include: [ { model: Product }]
+    });
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
-    // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
-});
+// router.get('/:id', async (req, res) => {
+//   const categoryData = await Category.findByPk(req.params.id, {
+//     include: [{}]
+//   });
+//   // find one category by its `id` value
+//   // be sure to include its associated Products
+// });
 
 router.post('/', (req, res) => {
   // create a new category
