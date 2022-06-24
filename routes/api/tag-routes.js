3bfcf9e9164cfbb260f9f519ddecf4eefaa946bd
Daniel Bowen <djbowen95@gmail.com>
2022-06-24 16:54:21 +0100

    When GET request recieved at api/tags, return data for all tags and associated products

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,11 +1,24 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
-// The `/api/tags` endpoint
+// Endpoint for all api/tags requests.
 
+// GET request without id: respond with all tags.
+// Also return the associated product data.
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+  try {
+    const tagData = Tag.findAll({
+      include: [{ model: Product}]
+    });
+
+    if (!tagData) {
+      return res.status(404).json({message:"No tag with this ID can be found."});
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
