87fdf72da6f7f9ba763945db56eadc96174ae51f
Daniel Bowen <djbowen95@gmail.com>
2022-06-24 15:55:59 +0100

    When PUT request recieved to api/categories with an :id, update the Category the request body

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -42,11 +42,42 @@ router.post("/", async (req, res) => {
 });
 
 router.put("/:id", async (req, res) => {
-  // update a category by its `id` value
+  try {
+    const categoryData = await Category.findByPk(req.params.id);
+
+    if (!categoryData) {
+      return res
+        .status(404)
+        .json({ message: "No category found with this ID." });
+    }
+
+    categoryData.update( req.body );
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete("/:id", async (req, res) => {
   // delete a category by its `id` value
 });
+
+// router.delete('/:id', async (req, res) => {
+//   try {
+//     const locationData = await Location.destroy({
+//       where: {
+//         id: req.params.id
+//       }
+//     });
+
+//     if (!locationData) {
+//       res.status(404).json({ message: 'No location found with this id!' });
+//       return;
+//     }
+
+//     res.status(200).json(locationData);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
 
 module.exports = router;
