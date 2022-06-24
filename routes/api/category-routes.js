279e98c397fb6ded97a7ca601e5956f8bbfcb951
Daniel Bowen <djbowen95@gmail.com>
2022-06-24 16:01:00 +0100

    When DELETE request recieved to api/categories  with an :id, delete the category with this id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,8 +1,7 @@
 const router = require("express").Router();
 const { Category, Product } = require("../../models");
 
-// The `/api/categories` endpoint
-
+// GET request with no id, retrieves all Category data with associated products
 router.get("/", async (req, res) => {
   try {
     const categoryData = await Category.findAll({
@@ -59,25 +58,21 @@ router.put("/:id", async (req, res) => {
 });
 
 router.delete("/:id", async (req, res) => {
-  // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      }
+    });
+
+    if (!categoryData) {
+      return res.status(404).json("No category with this ID exists to delete.");
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  };
 });
-
-// router.delete('/:id', async (req, res) => {
-//   try {
-//     const locationData = await Location.destroy({
-//       where: {
-//         id: req.params.id
-//       }
-//     });
-
-//     if (!locationData) {
-//       res.status(404).json({ message: 'No location found with this id!' });
-//       return;
-//     }
-
-//     res.status(200).json(locationData);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
 
 module.exports = router;
