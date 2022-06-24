0f22c968e8e7ee4d056c9917252562b7570df8b9
Daniel Bowen <djbowen95@gmail.com>
2022-06-24 16:24:53 +0100

    When GET request recieved to api/products, return all product data, and associated category and tags

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,17 +4,30 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try {
+    const productData = await Product.findAll({
+      include: [ {model: Category}, {model: Tag}]
+    }
+    )
+
+    if (!productData) {
+      return res.status(404).json( { message: "No product data found."})
+    }
+    
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  };
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
 });
 
+// Refactor this to async / await in future.
 // create new product
 router.post('/', (req, res) => {
   /* req.body should look like this...
@@ -89,7 +102,7 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
 });
 
