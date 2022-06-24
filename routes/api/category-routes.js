13bb471c55a17063a5f1b49aa8abf746b313c041
Daniel Bowen <djbowen95@gmail.com>
2022-06-24 15:42:55 +0100

    When POST request recieved to api/categories/, create a Category with the request body

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,12 +1,12 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const categoryData = await Category.findAll({
-      include: [ { model: Product }]
+      include: [{ model: Product }],
     });
     res.status(200).json(categoryData);
   } catch (err) {
@@ -14,32 +14,38 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   try {
     const categoryData = await Category.findByPk(req.params.id, {
-    include: [{ model: Product }]
-  });
+      include: [{ model: Product }],
+    });
 
-  if (!categoryData) {
-    return res.status(404).json({ message: "No category found with this ID." });
+    if (!categoryData) {
+      return res
+        .status(404)
+        .json({ message: "No category found with this ID." });
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
   }
-
-  res.status(200).json(categoryData);
-
-} catch (err) {
-  res.status(500).json(err);
-}
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post("/", async (req, res) => {
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete a category by its `id` value
 });
 
